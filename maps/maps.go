7677935fe3eb3349bacc4f8c2e3b6eb75cbbd281
michael <mcaplan@michaels-MBP.lan>
2022-01-23 13:12:50 -0500

    adding some maps

--- maps/maps.go
+++ maps/maps.go
@@ -1,8 +1,18 @@
 package main
 
+import "fmt"
+
+func divider() {
+	println("\n")
+}
 
 func main() {
-
-	print("fuck\n")
+	//  declare and define a map
+	map1 := map[string]string{"fuck": "you", "who": "me?"}
+	fmt.Printf("%s", map1)
+	divider()
+	for k, v := range map1 {
+		fmt.Printf("%s:%s\n", k, v)
+	}
 
 }
